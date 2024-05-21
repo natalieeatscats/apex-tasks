54a824e554978ec40e250802c47ebce5e90b9afd
Natalia Popova <93385001+natalieeatscats@users.noreply.github.com>
2024-05-21 15:38:17 +0300

    init, add user nest resource

---
+++ packages/server/src/auth/dto/auth.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, MinLength } from 'class-validator'
+
+export class AuthDto {
+	@IsString()
+	@MinLength(6, { message: 'Username must be at least 6 characters long' })
+	name: string;
+
+	@IsString()
+	@IsEmail()
+	email: string;
+
+	@IsString()
+	@MinLength(8, {
+		message: 'Password must be at least 8 characters long',
+	})
+	password: string;
+}
